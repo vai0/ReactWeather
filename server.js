49a244af232cb8e5694614a4b27b131611d508ca
Justin Chi <j.chi2241@gmail.com>
2016-05-14 01:20:28 -0700

    fix server.js file to work for Heroku deployment

--- server.js
+++ server.js
@@ -14,6 +14,6 @@ app.use(function(req, res, next) {
 
 app.use(express.static('public'));
 
-app.listen(3000, function() {
-  console.log('Express server is up on port 3000');
+app.listen(PORT, function() {
+  console.log('Express server is up on port ' + PORT);
 });
